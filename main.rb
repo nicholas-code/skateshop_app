47bb2928d0c937811a3a773812f68b6d777a2399
Nicholas Bauer <nicholasbauer89@gmail.com>
2020-09-27 16:07:34 +1000

    existing user and staff can log in, but cannot verify password yet!

--- main.rb
+++ main.rb
@@ -8,10 +8,35 @@ class Users
         @username = username
         @username = password
     end
+    
+
+    # end
+    
 end
 
-# class WelcomeOptions
 
+class PublicUsers < Users
+    def initialize
+        super(username, password)  
+        @options = ["View Stock"]
+    end
+    
+    class NewUser < Users
+        def initialize
+            super(username, password)
+        end
+        
+        def input
+            puts "What is your name?"
+            @username = gets.chomp
+            puts "Please make a password!"
+            @password = gets.chomp
+        end
+        
+    end
+end
+
+    # Start of application
     def start
         prompt = TTY::Prompt.new(symbols: {marker: '🛹'})
         system("clear")
@@ -21,31 +46,57 @@ end
         menu.choice 'Staff'
         menu.choice 'Exit'
         end
-
+    
         if welcome == 'New User'
-            puts 'ddd'
+            puts "What is your name?"
+            username = gets.chomp
+            puts "Please enter a password"
+            password = gets.chomp
+            puts "Please enter your email"
+            email = gets.chomp
+            CSV.open("login.csv", "a") do |csv|
+                csv << [username, password, email, "user"]
+                puts "You are now in the system"
+            end
+
         elsif welcome == 'Existing User'
-            puts 'ddds'
+            puts "What is your name?"
+            username = gets.chomp
+            csv = CSV.read( "login.csv", headers: true )
+            csv.find {|row| row['name'] == username}
+            if csv.find {|row| row['name'] == username}
+                puts "yeah nice #{username}, what is your password?"
+                password = gets.chomp
+                #fix me
+                           
+            else
+                puts "nah mate, you're not in the system. Try again!"
+                exit              
+            end
+
+
         elsif welcome == 'Staff'
-            puts 'hhh'
+            puts "What is your name?"
+            username = gets.chomp
+            csv = CSV.read( "login.csv", headers: true )
+            csv.find {|row| row['name'] == username}
+            if csv.find {|row| row['name'] == username}
+                puts "Hello #{username}, what is your password?"
+                password = gets.chomp
+                #fix me                             
+            else
+                puts "nah mate, you're not in the system. Try again!"
+                exit              
+            end
         else
             puts "Seeya"
             exit
         end
-
+    
         #puts @options
-    end
-# end
-
-class Public Users < Users
-    def initialize(username, password)
-        super(username, password)  
-        @options = ["View Stock", ]
     end
 
 puts start
-
-end
 # CSV.foreach('login.csv') do |row|
 #     puts row.inspect
 #   end
